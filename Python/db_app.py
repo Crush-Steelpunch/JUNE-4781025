d0098a4155f42257b4ffcba7030696742040bab3
Leon <leon.robinson@qa.com>
2023-08-10 14:53:31 +0100

    updated function to handle SELECTS and other directives

--- Python/db_app.py
+++ Python/db_app.py
@@ -9,3 +9,15 @@ for i in resultvar:
 showstaff = db_functionquery.dbquery("SELECT * FROM salesperson")
 for i in showstaff:
     print(i)
+
+insertquery = "INSERT INTO company (company_no,company_name,tel,county,post_code) VALUES (5000,'Leon Corp','01101 110 001','Gtr Manchester','M1 1AA')"
+
+query = db_functionquery.dbquery(insertquery)
+if query == None:
+    print('insert failed')
+else:
+    print('insert succeeded')
+
+resultvar = db_functionquery.dbquery("SELECT * FROM company")
+for i in resultvar:
+    print(i)
--- Python/db_functionquery.py
+++ Python/db_functionquery.py
@@ -3,6 +3,18 @@ def dbquery(statement):
     connectionString = r'DRIVER={ODBC Driver 13 for SQL Server};SERVER=.\SQLExpress;DATABASE=qastore;Trusted_Connection=yes'
     conn = pyodbc.connect(connectionString)
     cur = conn.cursor()
-    result = cur.execute(statement).fetchall()
-    conn.close()
-    return result
+    
+    try:
+        if 'SELECT' in statement:
+            result = cur.execute(statement).fetchall()
+        else:
+            cur.execute(statement)
+            conn.commit()
+            result = True
+        conn.close()
+        return result
+    except:
+        conn.close()
+        return None
+
+
