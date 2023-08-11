994d4b20cf81e8a75841b7706d8ee3cb96b5972f
Leon <leon.robinson@qa.com>
2023-08-11 14:01:07 +0100

    more tests

--- PythonUnitTesting/test_myapp/test_leonfunctions.py
+++ PythonUnitTesting/test_myapp/test_leonfunctions.py
@@ -2,3 +2,6 @@ import myapp.leonfunctions
 
 def test_something():
     assert myapp.leonfunctions.reverseaword('bees') == 'seeb'
+
+def test_seehearno():
+    assert myapp.leonfunctions.seenohearno('pies') == ["Hear No Seip","See No Pies", "Speak No Pies"]
--- PythonUnitTesting/test_myapp/test_myfunctions.py
+++ PythonUnitTesting/test_myapp/test_myfunctions.py
@@ -2,3 +2,6 @@ import myapp.myfunctions
 
 def test_myfunctions():
     assert myapp.myfunctions.maxthreenumbers(2,4,6) == 6
+    assert myapp.myfunctions.maxthreenumbers(6,4,2) == 6
+    assert myapp.myfunctions.maxthreenumbers(2,6,4) == 6
+    assert myapp.myfunctions.maxthreenumbers(4,2,6) == 6
